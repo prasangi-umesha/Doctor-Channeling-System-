69a79646c1bf5d2e0f62ec87500b88c078660ee3
Prasangi Samaranayaka <prasangiumesha@gmail.com>
2025-04-18 22:26:16 +0530

    Add files via upload

---
+++ firebaseauth.js
@@ -0,0 +1,16 @@
+firebaseauth.js 
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBfYK_PdAL-_wAmyZAEy9YvCosWcaZSzoo",
+  authDomain: "doctor-channeling-system.firebaseapp.com",
+  projectId: "doctor-channeling-system",
+  storageBucket: "doctor-channeling-system.appspot.com",
+  messagingSenderId: "1092523995119",
+  appId: "1:1092523995119:web:c989ad840af84d38ec501a"
+};
+
+// Initialize Firebase 
